7236a89a314b6af61a6eee8478706ee998dc623b
FoldFunc <fold.func@gmail.com>
2026-01-01 18:42:35 +0000

    Sending and removing messages.

--- main.go
+++ main.go
@@ -9,16 +9,20 @@ import (
 	"strconv"
 	"time"
 )
+var usedIds []int
 type Message struct {
-	Id int `json:"id"`
-	Body string `json:"body"`
+	Id     int    `json:"id"`
+	UserId int    `json:"user_id"`
+	Body   string `json:"body"`
 }
 type Room struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
+	Id       int       `json:"id"`
+	UserId   int       `json:"user_id"`
+	Name     string    `json:"name"`
+	Messages []Message `json:"messages"`
 }
 type User struct {
-	Id int `json:"id"`
+	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 type GlobalMessages struct {
@@ -31,18 +35,43 @@ type GlobalRooms struct {
 	Rooms []Room `json:"rooms"`
 }
 type NewRoomReq struct {
-	RoomId int `json:"room_id"`
+	RoomId   int    `json:"room_id"`
 	RoomName string `json:"room_name"`
-	UserId int `json:"user_id"`
+	UserId   int    `json:"user_id"`
 }
 type NewUserReq struct {
 	UserName string `json:"user_name"`
+}
+type SendMessreq struct {
+	RoomId int    `json:"room_id"`
+	UserId int    `json:"user_id"`
+	Body   string `json:"body"`
+}
+type RemovemesReq struct {
+	RoomId int    `json:"room_id"`
+	UserId int    `json:"user_id"`
+	MessId int    `json:"mess_id"`
 }
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Println("/ handler called")
 }
 func generateId() (int) {
-	return rand.IntN(1 << 32)
+	var cont bool
+	var newId int
+	for {
+		cont = false
+		newId = rand.IntN(1 << 32)
+		for _, n := range usedIds {
+			if n == newId {
+				cont = true
+			}
+		}
+		if !cont {
+			break
+		}
+	}
+	usedIds = append(usedIds, newId)
+	return newId
 }
 func newUser(w http.ResponseWriter, r *http.Request) {
 	log.Println("/newUser called")
@@ -73,6 +102,34 @@ func newUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func messageExsists(room_id, mess_id, user_id int) (bool) {
+	for _, r := range R.Rooms {
+		if r.Id == room_id {
+			for _, m := range r.Messages {
+				if m.Id == mess_id && m.UserId == user_id {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+func roomExsists(id int) (bool) {
+	for _, u := range R.Rooms {
+		if u.Id == id {
+			return true
+		}
+	}
+	return false
+}
+func roomExsistsToDelete(id, user_id int) (bool) {
+	for _, u := range R.Rooms {
+		if u.Id == id && u.UserId == user_id {
+			return true
+		}
+	}
+	return false
+}
 func userExsists(id int) (bool) {
 	for _, u := range U.Users {
 		if u.Id == id {
@@ -102,6 +159,7 @@ func newRoom(w http.ResponseWriter, r *http.Request) {
 	newId := generateId()
 	NewRoom := Room{
 		Id: newId,
+		UserId: req.UserId,
 		Name: req.RoomName,
 	}
 	for _, r := range R.Rooms {
@@ -154,8 +212,78 @@ func getRoom(w http.ResponseWriter, r *http.Request) {
 		"id": returnRoom.Id,
 	})
 }
+func sendMessage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only get requests allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var req SendMessreq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	if !userExsists(req.UserId) {
+		http.Error(w, "User verification failed", http.StatusForbidden)
+		return
+	}
+	if !roomExsists(req.RoomId) {
+		http.Error(w, "No such room", http.StatusNotFound)
+		return 
+	}
+	id := generateId()
+	message := Message{
+		Id: id,
+		UserId: req.UserId,
+		Body: req.Body,
+	}
+	for _, r := range R.Rooms {
+		if r.Id == req.RoomId {
+			r.Messages = append(r.Messages, message)
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "message created",
+		"id": string(id),
+	})
+}
+func removeMessage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	var req RemovemesReq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	if !userExsists(req.UserId) {
+		http.Error(w, "User verification failed", http.StatusForbidden)
+		return
+	}
+	if !roomExsists(req.RoomId) {
+		http.Error(w, "No such room", http.StatusBadRequest)
+		return
+	}
+	if !messageExsists(req.RoomId, req.MessId, req.UserId) {
+		http.Error(w, "No such message", http.StatusBadRequest)
+		return
+	}
+	for _, r := range R.Rooms {
+		if r.Id == req.RoomId {
+			for i, m := range r.Messages {
+				if m.Id == req.MessId {
+					r.Messages = append(r.Messages[:i], r.Messages[i+1:]...)
+				}
+			}
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "message removed",
+	})
+}
 var R GlobalRooms
-var M GlobalMessages 
 var U GlobalUsers
 func main() {
 	mux := http.NewServeMux()
@@ -163,6 +291,8 @@ func main() {
 	mux.HandleFunc("/newUser", newUser)
 	mux.HandleFunc("/newRoom", newRoom)
 	mux.HandleFunc("/getRoom", getRoom)
+	mux.HandleFunc("/sendMessage", sendMessage)
+	mux.HandleFunc("/removeMessage", removeMessage)
 	server := &http.Server{
 		Addr: ":42069",
 		Handler: mux,
