c7402682cb72c514bcfbc001e09a19134190bad6
FoldFunc <fold.func@gmail.com>
2026-01-02 03:21:52 +0000

    Public and private rooms

--- main.go
+++ main.go
@@ -1,7 +1,4 @@
 /*
-Make the handler that would delete the rooms
-But only let the users that created the rooms to delete them.
-
 Also maybe make private and open rooms
 So to some of them you can only get access if someone assgins you there.
 */
@@ -15,6 +12,7 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+
 )
 var usedIds []int
 type Message struct {
@@ -22,15 +20,24 @@ type Message struct {
 	UserId int    `json:"user_id"`
 	Body   string `json:"body"`
 }
+type Type string
+const (
+	TypePublic  Type = "public"
+	TypePrivate Type = "private"
+)
 type Room struct {
 	Id       int       `json:"id"`
 	UserId   int       `json:"user_id"`
 	Name     string    `json:"name"`
 	Messages []Message `json:"messages"`
+	Type     Type      `json:"type"`
+	Admins   []User    `json:"admins"`
+	Users    []User    `json:"users"`
 }
 type User struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Id    int     `json:"id"`
+	Name  string  `json:"name"`
+	Rooms []Room `json:"rooms"`
 }
 type GlobalMessages struct {
 	Messages []Message `json:"messages"`
@@ -45,6 +52,7 @@ type NewRoomReq struct {
 	RoomId   int    `json:"room_id"`
 	RoomName string `json:"room_name"`
 	UserId   int    `json:"user_id"`
+	RoomType bool   `json:"room_type"`
 }
 type NewUserReq struct {
 	UserName string `json:"user_name"`
@@ -62,6 +70,15 @@ type RemovemesReq struct {
 type RemoveRoomReq struct {
 	RoomId int    `json:"room_id"`
 	UserId int    `json:"user_id"`
+}
+type AddToCloseRoomReq struct {
+	RoomId  int `json:"room_id"`
+	AdminId int `json:"admin_id"`
+	UserId  int `json:"user_id"`
+}
+type AccesRoomReq struct {
+	RoomId int `json:"room_id"`
+	UserId int `json:"user_id"`
 }
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Println("/ handler called")
@@ -113,6 +130,18 @@ func newUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func IsAdmin(user_id, room_id int) (bool) {
+	for _, r := range R.Rooms {
+		if r.Id == room_id {
+			for _, a := range r.Admins {
+				if a.Id == user_id {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
 func messageExsists(room_id, mess_id, user_id int) (bool) {
 	for _, r := range R.Rooms {
 		if r.Id == room_id {
@@ -121,6 +150,14 @@ func messageExsists(room_id, mess_id, user_id int) (bool) {
 					return true
 				}
 			}
+		}
+	}
+	return false
+}
+func roomPublic(id int) (bool) {
+	for _, u := range R.Rooms {
+		if u.Id == id && u.Type == TypePublic {
+			return true
 		}
 	}
 	return false
@@ -149,6 +186,26 @@ func userExsists(id int) (bool) {
 	}
 	return false
 }
+func UserInRoom(user_id, room_id int) (bool) {
+	for _, u := range U.Users {
+		if u.Id == user_id {
+			for _, r := range u.Rooms {
+				if r.Id == room_id {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+func getUserById(user_id int) (User, error) {
+	for _, u := range U.Users {
+		if u.Id == user_id {
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("Invalid user id")
+}
 func newRoom(w http.ResponseWriter, r *http.Request) {
 	log.Println("/newRoom called")
 	if r.Method != http.MethodPost {
@@ -167,14 +224,32 @@ func newRoom(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "User validation failed", http.StatusForbidden)
 		return
 	}
+	var roomType Type
+	if req.RoomType {
+		roomType = Type(TypePublic)
+	} else {
+		roomType = Type(TypePrivate)
+	}
 	newId := generateId()
+	currentUser, err := getUserById(req.UserId)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	var admins []User
+	var users  []User
+	admins = append(admins, currentUser)
+	users = append(users, currentUser)
 	NewRoom := Room{
 		Id: newId,
 		UserId: req.UserId,
 		Name: req.RoomName,
+		Type: roomType,
+		Admins: admins,
+		Users: users,
 	}
 	for _, r := range R.Rooms {
-		fmt.Printf("Room id: %d;Room name: %s\n", newId, r.Name)
+		fmt.Printf("Room id: %d;Room name: %s;Room public: %v\n", newId, r.Name, r.Type)
 		if req.RoomName == r.Name {
 			http.Error(w, "Room name already exsists", http.StatusConflict)
 			return
@@ -186,6 +261,7 @@ func newRoom(w http.ResponseWriter, r *http.Request) {
 		"id": newId,
 	})
 }
+// Depricated
 func getRoom(w http.ResponseWriter, r *http.Request) {
 	log.Println("/getRoom called")
 	if r.Method != http.MethodGet {
@@ -217,13 +293,15 @@ func getRoom(w http.ResponseWriter, r *http.Request) {
 			returnRoom = r
 		}
 	}
+	idInt := strconv.Itoa(returnRoom.Id)
 	fmt.Printf("Room id requested: %d\n", returnRoom.Id)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]int{
-		"id": returnRoom.Id,
+	json.NewEncoder(w).Encode(map[string]string{
+		"id": idInt,
+		"type": string(returnRoom.Type),
 	})
 }
-func sendMessage(w http.ResponseWriter, r *http.Request) {
+func sendMessageCloseRoom(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only get requests allowed", http.StatusMethodNotAllowed)
 		return
@@ -242,6 +320,14 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "No such room", http.StatusNotFound)
 		return 
 	}
+	if roomPublic(req.RoomId) {
+		http.Error(w, "Room public", http.StatusForbidden)
+		return
+	}
+	if !UserInRoom(req.UserId, req.RoomId) {
+		http.Error(w, "You don't belong to this room", http.StatusForbidden)
+		return
+	}
 	id := generateId()
 	message := Message{
 		Id: id,
@@ -253,9 +339,50 @@ func sendMessage(w http.ResponseWriter, r *http.Request) {
 			r.Messages = append(r.Messages, message)
 		}
 	}
+	idInt := strconv.Itoa(id)
 	json.NewEncoder(w).Encode(map[string]string{
 		"message": "message created",
-		"id": string(id),
+		"id": idInt,
+	})
+}
+func sendMessageOpenRoom(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only get requests allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var req SendMessreq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	if !userExsists(req.UserId) {
+		http.Error(w, "User verification failed", http.StatusForbidden)
+		return
+	}
+	if !roomExsists(req.RoomId) {
+		http.Error(w, "No such room", http.StatusNotFound)
+		return 
+	}
+	if !roomPublic(req.RoomId) {
+		http.Error(w, "Room not public", http.StatusForbidden)
+		return
+	}
+	id := generateId()
+	message := Message{
+		Id: id,
+		UserId: req.UserId,
+		Body: req.Body,
+	}
+	for _, r := range R.Rooms {
+		if r.Id == req.RoomId {
+			r.Messages = append(r.Messages, message)
+		}
+	}
+	idInt := strconv.Itoa(id)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "message created",
+		"id": idInt,
 	})
 }
 func removeMessage(w http.ResponseWriter, r *http.Request) {
@@ -322,6 +449,77 @@ func removeRoom(w http.ResponseWriter, r *http.Request) {
 		"message": "Room deleted",
 	})
 }
+func addToCloseRoom(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	var req AddToCloseRoomReq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return 
+	}
+	if !userExsists(req.UserId) {
+		http.Error(w, "User verification failed", http.StatusNotFound)
+		return
+	}
+	if !userExsists(req.AdminId) {
+		http.Error(w, "Admin verification failed", http.StatusNotFound)
+		return
+	}
+	if !IsAdmin(req.AdminId, req.RoomId) {
+		http.Error(w, "AdminId is not an admin", http.StatusForbidden)
+		return
+	}
+	currentUser, err := getUserById(req.UserId)
+	if err != nil {
+		http.Error(w, "User not found", http.StatusForbidden)
+		return
+	}
+	for _, r := range R.Rooms {
+		if r.Id == req.RoomId {
+			r.Users = append(r.Users, currentUser)
+		}
+	}
+}
+func accesRoom(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	var req AccesRoomReq
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	if !userExsists(req.UserId) {
+		http.Error(w, "User verification failed", http.StatusForbidden)
+		return
+	}
+	if !roomExsists(req.RoomId) {
+		http.Error(w, "Room does not exsist", http.StatusBadRequest)
+		return
+	}
+	if !roomPublic(req.RoomId) {
+		http.Error(w, "Room is not public", http.StatusBadRequest)
+		return
+	}
+	currentUser, err := getUserById(req.UserId) 
+	if err != nil {
+		http.Error(w, "No such user", http.StatusForbidden)
+		return
+	}
+	for _, r := range R.Rooms {
+		if r.Id == req.RoomId {
+			r.Users = append(r.Users, currentUser)
+		}
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "User added to the room",
+	})
+}
 var R GlobalRooms
 var U GlobalUsers
 func main() {
@@ -330,7 +528,10 @@ func main() {
 	mux.HandleFunc("/newUser", newUser)
 	mux.HandleFunc("/newRoom", newRoom)
 	mux.HandleFunc("/getRoom", getRoom)
-	mux.HandleFunc("/sendMessage", sendMessage)
+	mux.HandleFunc("/accesRoom", accesRoom)
+	mux.HandleFunc("/sendMessageOpenRoom", sendMessageOpenRoom)
+	mux.HandleFunc("/sendMessageCloseRoom", sendMessageCloseRoom)
+	mux.HandleFunc("/addToCloseRoom", addToCloseRoom)
 	mux.HandleFunc("/removeMessage", removeMessage)
 	mux.HandleFunc("/removeRoom", removeRoom)
 	server := &http.Server{
