1a2ecd46fe8b5a6a517bd7c23e250d91eb8f2399
FoldFunc <fold.func@gmail.com>
2026-01-14 16:32:01 +0000

    Frontend sucks.

---
+++ frontend/src/assets/mainPage.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './mainPage.css'
+
+function MainPage() {
+  return(
+    <div>
+      <h1 class="title">Hello to the newest chat service.</h1>
+    </div>
+  )
+}
+export default MainPage
